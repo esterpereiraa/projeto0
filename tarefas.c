991968e83f26697ba337bb033ec82d65103cc905
esterpereiraa <161087859+esterpereiraa@users.noreply.github.com>
2024-04-01 05:04:05 +0000

    feat: função de carregar os arquivos

--- tarefas.c
+++ tarefas.c
@@ -70,7 +70,19 @@ ERROS salvar(Tarefa tarefas[], int *pos ){
     return OK;
 }
 ERROS carregar(Tarefa tarefas[], int *pos ){
-    printf("Funçao de carregar tarefas\n");
+     FILE *f = fopen("tarefas.bin","rb");
+    if (f == NULL)
+        return ABRIR;
+    int qtd = fread(tarefas, TOTAL, sizeof(Tarefa),f);
+    if(qtd ==0)
+        return LER;
+    
+    qtd = fread(pos,1,sizeof(int),f);
+    if (qtd ==0)
+        return LER;
+
+    if (fclose(f))
+        return FECHAR;
     return OK;
 }
 
