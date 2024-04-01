aeccf5d2d7be3c3524dc240211920152540464c6
esterpereiraa <161087859+esterpereiraa@users.noreply.github.com>
2024-04-01 02:07:08 +0000

    feat: defini

---
+++ tarefas.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "tarefas.h"
+
+ERROS criar(Tarefa tarefas[], int *pos ){
+    printf("Funçao de criar tarefas\n");
+    return OK;
+}
+ERROS deletar(Tarefa tarefas[], int *pos ){
+    printf("Funçao de deletar tarefas\n");
+    return OK;
+}
+ERROS listar(Tarefa tarefas[], int *pos ){
+    printf("Funçao de listar tarefas\n");
+    return OK;}
+ERROS salvar(Tarefa tarefas[], int *pos ){
+    printf("Funçao de salvar tarefas\n");
+    return OK;
+}
+ERROS carregar(Tarefa tarefas[], int *pos ){
+    printf("Funçao de carregar tarefas\n");
+    return OK;
+}
